6f9f00fee47d0ded29584fc73e9077bfeb4a492c
Ehsan Fani <ronaldo_esi@yahoo.com>
2026-04-18 17:57:55 +0200

    Added Service

---
+++ frontend/src/Services/userServices.js
@@ -0,0 +1,40 @@
+const API_URL = 'http://127.0.0.1:8000/users'
+
+async function request(url, options = {}) {
+    const res = await fetch(url, options)
+
+    if (!res.ok) {
+        const data = await res.json().catch(() => ({}))
+        throw new Error(data.detail || 'Request failed')
+    }
+
+    return res.status === 204 ? null : res.json()
+}
+
+export default {
+    getAllUsers() {
+        return request(API_URL)
+    },
+
+    createUser(user) {
+        return request(API_URL, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(user),
+        })
+    },
+
+    updateUser(id, user) {
+        return request(`${API_URL}/${id}`, {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(user),
+        })
+    },
+
+    deleteUser(id) {
+        return request(`${API_URL}/${id}`, {
+            method: 'DELETE',
+        })
+    },
+}
